42b077cedc4d99ab0dee1cc28b5ec45e80e4fc1f
Rahul Jangir <78272136+rahuljngr@users.noreply.github.com>
2021-12-01 14:15:16 +0530

    Update settings.py
    change secret_key

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s_-^@j5$jv4chwbvdwvy)cdjnlx5c%xpiltslih#1#3l#0--pc'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =True
