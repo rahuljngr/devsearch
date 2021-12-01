d054ff167159832b16bad9e4b60b7a64e3b4d992
Rahul Jangir <78272136+rahuljngr@users.noreply.github.com>
2021-12-01 14:19:34 +0530

    Update settings.py
    secure bucket id

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -209,7 +209,7 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = 'devsearch-bucket12'
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 if os.getcwd() == '/app':
     DEBUG = False
